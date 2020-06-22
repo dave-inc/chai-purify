d864763ecdeca71c426ef260b99b13d966dadc55
Chen Guo <chen.guo.0625@gmail.com>
2020-06-22 11:06:18 -0700

    improve maybe asserts

--- src/maybe.ts
+++ src/maybe.ts
@@ -4,42 +4,38 @@ import { Just, Maybe, Nothing } from 'purify-ts/Maybe';
 declare global {
   export namespace Chai {
     interface Assertion {
-      Just(value: any): Assertion;
+      Just<T>(value?: T): Assertion;
       Nothing: Assertion;
     }
   }
 }
 
 Assertion.addProperty('Nothing', function() {
-  const obj = util.flag(this, 'object');
   this.assert(
-    obj === Nothing,
-    `expected ${obj} to be Nothing`,
-    `expected ${obj} to not be Nothing`,
+    this._obj === Nothing,
+    `expected #{this} to be Nothing`,
+    `expected #{this} to not be Nothing`,
     Nothing,
-    obj,
+    this._obj,
     true
   );
 });
 
-Assertion.addChainableMethod('Just', function(value: any) {
-  const obj = util.flag(this, 'object');
-  this.assert(
-    Just(value).equals(obj),
-    `expected ${obj} to be ${Just(value)}`,
-    `expected ${obj} to not be ${Just(value)}`,
-    Just(value),
-    obj,
-    true
-  );
-}, function () {
-  const obj = util.flag(this, 'object');
-  this.assert(
-    obj?.constructor === Maybe && (obj as Maybe<unknown>).isJust(),
-    `expected ${obj} to be a Just`,
-    `expected ${obj} to not be a Just}`,
-    Just,
-    obj,
-    true
-  );
+Assertion.addMethod('Just', function<T>(value?: T) {
+  const obj = this._obj;
+  if (arguments.length === 0) {
+    this.assert(
+      obj.constructor === Maybe && obj.isJust(),
+      `expected #{this} to be a Just`,
+      `expected #{this} to not be a Just`,
+      Just(value),
+    );
+  } else {
+    this.assert(
+      Just(value).equals(obj as Maybe<T>),
+      `expected #{this} to be ${Just(value)}`,
+      `expected #{this} to not be ${Just(value)}`,
+      Just(value),
+    );
+  }
 });
--- test/maybe.ts
+++ test/maybe.ts
@@ -16,18 +16,21 @@ describe('Maybe', () => {
   });
 
   it('should check for a Just', () => {
-    expect(Just(5)).to.be.Just;
+    expect(Just(5)).to.be.Just();
+    expect(5).to.not.be.Just();
     expect(Maybe.fromNullable('foo')).to.be.Just;
+    expect(Just(5).map(v => v)).to.be.Just;
   });
 
   it('should check for not a Just', () => {
     expect(Nothing).to.not.be.Just;
     expect([]).to.not.be.Just;
-    expect(Maybe.fromNullable(null)).to.not.be.Just;
   });
 
   it('should check for a Just with value', () => {
     expect(Just(5)).to.be.Just(5);
+    expect(Just(5)).to.not.be.Just('foo');
+    expect('foo').to.not.be.Just('foo');
     expect(Maybe.fromNullable('foo')).to.be.Just('foo');
   });
 
